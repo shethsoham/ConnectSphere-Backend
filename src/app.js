f18767768a2d93b46aeac0de16912748ee4823c5
Soham Sheth <sohamashishsheth@gmail.com>
2025-02-18 19:00:50 -0600

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -1,17 +1,28 @@
-const express = require('express');
+const express = require("express");
 
-const app =express();
+const app = express();
 
-//request handler
-app.use("/",(req,res)=>{
-    res.send("Hello from slash");
+// This will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Soham", lastName: "Sheth" });
 });
-app.use("/test",(req,res)=>{
-    res.send("Hello from test");
+
+app.post("/user", async (req, res) => {
+  console.log(req.body);
+  // saving data to DB
+  res.send("Data successfully saved to the database!");
 });
-app.use("/hello",(req,res)=>{
-    res.send("Hello from ");
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully!");
 });
-app.listen(3000,()=>{
-    console.log("Server is successfully running on port 3000...")
+
+// this will match all the HTTP method API calls to /test
+app.use("/test", (req, res) => {
+  res.send("Hello from the server!");
+});
+
+
+app.listen(7777, () => {
+  console.log("Server is successfully listening on port 7777...");
 });
